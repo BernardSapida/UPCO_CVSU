eebdf898bb09bad741b2fd40779b1126dbc31f1f
Bernard Sapida <bernardsapida1706@gmail.com>
2025-01-30 12:43:17 +0800

    feat(clean): Remove navbar in layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,4 @@
 import { checkAuth } from '@/lib/auth/utils';
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
 import { ClerkProvider } from '@clerk/nextjs';
 import TrpcProvider from '@/lib/trpc/Provider';
 import { cookies } from 'next/headers';
@@ -20,11 +18,7 @@ export default async function AppLayout({ children }: { children: React.ReactNod
                      showSpinner={false}
                   />
                   <div className="flex h-screen">
-                     <Sidebar />
-                     <main className="flex-1 overflow-y-auto p-8 pt-2 md:p-8">
-                        <Navbar />
-                        {children}
-                     </main>
+                     <main className="flex-1 overflow-y-auto p-8 pt-2 md:p-8">{children}</main>
                   </div>
                </TrpcProvider>
             </ClerkProvider>
